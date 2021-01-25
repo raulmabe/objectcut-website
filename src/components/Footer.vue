6c5ca962a36d4b278ca8a201fe8ea2869f0bf5e5
Raul Mabe <raulmateob@gmail.com>
2021-01-25 19:53:03 +0100

    :lipstick: :recycle:

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <p>
       <strong>API</strong> by
       <a href="https://objectcut.com/">ObjectCut ✂️</a> and
-      <strong>website</strong> by <a href="https://raulmabe.dev">Raul Mabe</a>.
+      <strong>demo</strong> by <a href="https://raulmabe.dev">Raul Mabe</a>.
       The source code is licensed
       <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
       website content is licensed
@@ -12,22 +12,6 @@
       >.
     </p>
   </footer>
-  <!-- <footer class="footer has-background-white">
-    <div class="content has-text-centered">
-      <p>
-        <strong>API</strong> by
-        <a href="https://objectcut.com/">ObjectCut ✂️</a> and
-        <strong>demo</strong> by
-        <a href="https://raulmabe.dev">Raul Mabe ☕️</a>. The source code is
-        licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
-        website content is licensed
-        <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
-          >CC BY NC SA 4.0</a
-        >.
-      </p>
-    </div>
-  </footer> -->
 </template>
 
 <script>
